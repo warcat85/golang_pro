cb6e5c6f76a6792b5fe818934cfa5aa15cde4ffa
Александр Марамзин <ao.maramzin@jet.su>
2025-06-30 21:49:03 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,90 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isDigit(r rune) bool {
+	return r >= '0' && r <= '9'
+}
+
+func isEscape(r rune) bool {
+	return r == '\\'
+}
+
+func Unpack(source string) (string, error) {
+	var resp strings.Builder
+	resp.Grow(len(source))
+	var startIndex int // index of the first character to copy
+	var endIndex int   // index beyond the last character to copy
+	var last rune      // last character value
+
+	wasDigit := false
+	wasEscape := false
+	for index, char := range source {
+		isEscape := isEscape(char)
+		isDigit := isDigit(char)
+
+		// if the previous character was an escape
+		if wasEscape {
+			// make sure the current character is escapable
+			if !isEscape && !isDigit {
+				return "", ErrInvalidString
+			}
+			// escape
+			isEscape, isDigit = false, false
+		}
+
+		// if current character is a digit
+		if isDigit {
+			// if the first character of the string or follows the digit
+			if index == 0 || wasDigit {
+				return "", ErrInvalidString
+			}
+		}
+
+		if wasEscape || isDigit {
+			// write string up to a symbol before the digit or previous escape
+			resp.WriteString(source[startIndex:endIndex])
+		}
+
+		if isDigit {
+			// write previous character digit number of times
+			digit := int(char - '0')
+			if digit > 0 {
+				resp.WriteString(strings.Repeat(string(last), digit))
+			}
+		} else {
+			// current character is not a digit
+			// if we previously escaped or wrote a character digit of times
+			if wasEscape || wasDigit {
+				// start from current position again
+				startIndex = index
+			}
+			// save last index
+			endIndex = index
+			if !isEscape {
+				// save last character if not escape
+				// we may multiply it next if it isn't
+				last = char
+			}
+		}
+
+		// if current character is special, save which one
+		wasEscape = isEscape
+		wasDigit = isDigit
+	}
+
+	// ending with escape
+	if wasEscape {
+		return "", ErrInvalidString
+	}
+
+	// copy rest of the string if the last character wasn't a digit
+	if !wasDigit {
+		resp.WriteString(source[startIndex:])
+	}
+	return resp.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,15 +18,21 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
+		{input: "a", expected: "a"},
+		{input: "a3", expected: "aaa"},
+		{input: "a3b", expected: "aaab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `\\abc`, expected: `\abc`},
+		{input: `\3abc`, expected: `3abc`},
+		{input: `abc\3`, expected: `abc3`},
+		{input: `abc\\`, expected: `abc\`},
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +42,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "3a", "a33", `a\a`, `b\`}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
