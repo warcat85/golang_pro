4cbe7666261495fb80a2619eb676bda4b5b31229
Александр Марамзин <ao.maramzin@jet.su>
2026-01-26 09:15:25 +0300

    Rearranged the code Removed logging Swapped NewCache parameters Updated runner parameters Added benchmarks

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,8 +2,6 @@ package hw04lrucache
 
 import (
 	"fmt"
-	"math/rand"
-	"strings"
 	"sync"
 )
 
@@ -46,53 +44,18 @@ type lruCache struct {
 	queue    List
 }
 
-type lruSafeCache struct {
-	*lruCache
-	muMain  sync.RWMutex
-	muQueue sync.Mutex
-}
-
-type lruCacheSingleLock struct {
-	*lruSafeCache
-}
-
-type lruCacheDoubleLock struct {
-	*lruSafeCache
-}
-
-// CacheTripleLock is not working correctly if the following is true:
-// - number of possible values is less or equal the number of setter goroutines,
-// - and capacity is less than number of possible values
-// So if capacity < values <= setter goroutines it can happen that at some point the cache will contain
-// all the values (which will be more than capacity).
-type lruCacheTripleLock struct {
-	*lruSafeCache
-}
-
 // ItemCaches below can only cache single item.
 type lruItemCache struct {
 	*lruCache
 	helper lruItemHelper
 }
 
-type lruItemCacheSingleLock struct {
-	*lruSafeCache
-	helper lruItemHelper
-}
-
-type lruItemCacheDoubleLock struct {
-	*lruSafeCache
-	helper lruItemHelper
-}
-
-type lruItemHelper struct{}
-
 type elem struct {
 	key   Key
 	value interface{}
 }
 
-func NewCache(capacity int, kind Kind) Cache {
+func NewCache(kind Kind, capacity int) Cache {
 	lruCache := &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
@@ -113,6 +76,7 @@ func NewCache(capacity int, kind Kind) Cache {
 	}
 }
 
+// cache that can only store single item (for testing performance)
 func NewItemCache(kind Kind) Cache {
 	lruCache := &lruCache{
 		capacity: 1,
@@ -143,7 +107,9 @@ func (c *lruCache) Set(key Key, value interface{}) bool {
 		return true
 	}
 
-	c.removeLastItemIfFull()
+	if c.queue.Len() == c.capacity {
+		c.removeLastItem()
+	}
 	c.addItem(key, value)
 	return false
 }
@@ -179,13 +145,6 @@ func val(item *ListItem) *elem {
 	return item.Value.(*elem)
 }
 
-func (c *lruCache) removeLastItemIfFull() {
-	queue := c.queue
-	if queue.Len() == c.capacity {
-		c.removeLastItem()
-	}
-}
-
 func (c *lruCache) removeLastItem() {
 	queue := c.queue
 	back := queue.Back()
@@ -196,146 +155,6 @@ func (c *lruCache) removeLastItem() {
 func (c *lruCache) addItem(key Key, value interface{}) {
 	elem := c.queue.PushFront(&elem{key, value})
 	c.items[key] = elem
-}
-
-func (c *lruSafeCache) Get(key Key) (interface{}, bool) {
-	c.muMain.RLock()
-	defer c.muMain.RUnlock()
-	item := c.items[key]
-	if item != nil {
-		c.muQueue.Lock()
-		defer c.muQueue.Unlock()
-		if len(c.items) > c.capacity {
-			fmt.Printf("[%s] -> moving on get! (queue [%s], items [%s])\n",
-				key, printQueue(c.queue), printMap(c.items))
-		}
-		return c.lruCache.getItem(item), true
-	}
-	return nil, false
-}
-
-func (c *lruSafeCache) Clear() {
-	c.muMain.Lock()
-	if len(c.items) > c.capacity {
-		fmt.Printf("[CACHE] -> clearing! (queue [%s], items [%s])\n",
-			printQueue(c.queue), printMap(c.items))
-	}
-	c.lruCache.Clear()
-	c.muMain.Unlock()
-}
-
-func (c *lruSafeCache) removeLastItem(id int, key Key) {
-	queue := c.queue
-	back := queue.Back()
-	rkey := val(back).key
-	if len(c.items) > c.capacity {
-		fmt.Printf("(%07d) [%s] -> removing (%s)! (queue [%s], items [%s])\n",
-			id, key, rkey, printQueue(c.queue), printMap(c.items))
-	}
-	queue.Remove(back)
-	delete(c.items, rkey)
-}
-
-func (c *lruCacheSingleLock) Set(key Key, value interface{}) bool {
-	id := rand.Intn(1_000_000)
-	// we need to make sure when we move to front item is not deleted from queue
-	c.muMain.Lock()
-	item := c.items[key]
-	if item != nil {
-		// when deleting we need to make sure muMap is locked
-		if len(c.items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> moving on set! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
-		c.lruCache.setItem(item, value)
-		c.muMain.Unlock()
-		return true
-	}
-
-	if c.queue.Len() == c.capacity {
-		c.removeLastItem(id, key)
-	}
-	c.addItem(key, value)
-	if len(c.items) > c.capacity {
-		fmt.Printf("(%07d) [%s] -> added! (queue [%s], items [%s])\n",
-			id, key, printQueue(c.queue), printMap(c.items))
-	}
-	c.muMain.Unlock()
-	return false
-}
-
-func (c *lruCacheDoubleLock) Set(key Key, value interface{}) bool {
-	id := rand.Intn(1_000_000)
-	// we need to make sure when we move to front item is not deleted from queue
-	c.muMain.RLock()
-	item := c.items[key]
-	if item != nil {
-		// when deleting we need to make sure muMap is locked
-		c.muQueue.Lock()
-		if len(c.items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> moving on set! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
-		c.lruCache.setItem(item, value)
-		c.muQueue.Unlock()
-		c.muMain.RUnlock()
-		return true
-	}
-	c.muMain.RUnlock()
-
-	c.muMain.Lock()
-	for c.queue.Len() >= c.capacity {
-		c.removeLastItem(id, key)
-	}
-
-	items := c.items
-	if _, exists := items[key]; !exists {
-		c.addItem(key, value)
-		if len(items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> added! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
-	}
-	c.muMain.Unlock()
-	return false
-}
-
-func (c *lruCacheTripleLock) Set(key Key, value interface{}) bool {
-	id := rand.Intn(1_000_000)
-	// we need to make sure when we move to front item is not deleted from queue
-	c.muMain.RLock()
-	item := c.items[key]
-	if item != nil {
-		// when deleting we need to make sure muMap is locked
-		c.muQueue.Lock()
-		if len(c.items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> moving on set! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
-		c.lruCache.setItem(item, value)
-		c.muQueue.Unlock()
-		c.muMain.RUnlock()
-		return true
-	}
-	c.muMain.RUnlock()
-
-	c.muMain.Lock()
-	for c.queue.Len() >= c.capacity {
-		c.removeLastItem(id, key)
-	}
-	c.muMain.Unlock()
-
-	c.muMain.Lock()
-	items := c.items
-	if _, exists := items[key]; !exists {
-		c.addItem(key, value)
-		if len(items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> added! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
-	}
-	c.muMain.Unlock()
-	return false
 }
 
 func (c *lruItemCache) Set(key Key, value interface{}) bool {
@@ -355,40 +174,87 @@ func (h *lruItemHelper) clearAndAddItem(c *lruCache, key Key, value interface{})
 	c.items = map[Key]*ListItem{key: elem}
 }
 
-func (c *lruItemCacheSingleLock) Set(key Key, value interface{}) bool {
-	id := rand.Intn(1_000_000)
-	// we need to make sure when we move to front item is not deleted from queue
-	c.muMain.Lock()
-	defer c.muMain.Unlock()
+/* Below is the completed task with asterisk.*/
+type lruSafeCache struct {
+	*lruCache
+	muMain  sync.RWMutex
+	muQueue sync.Mutex
+}
+
+type lruCacheSingleLock struct {
+	*lruSafeCache
+}
+
+type lruCacheDoubleLock struct {
+	*lruSafeCache
+}
+
+// CacheTripleLock is not working correctly if the following is true:
+// - number of possible values is less or equal the number of setter goroutines,
+// - and capacity is less than number of possible values
+// So if (capacity < values <= setter goroutines) it can happen that at some point the cache will
+// contain all the values (which will be more than capacity)
+// it should be also noted that this cache may temporarily hold a bit more values than the capacity
+type lruCacheTripleLock struct {
+	*lruSafeCache
+}
+
+type lruItemCacheSingleLock struct {
+	*lruSafeCache
+	helper lruItemHelper
+}
+
+type lruItemCacheDoubleLock struct {
+	*lruSafeCache
+	helper lruItemHelper
+}
+
+type lruItemHelper struct{}
+
+func (c *lruSafeCache) Get(key Key) (interface{}, bool) {
+	c.muMain.RLock()
+	defer c.muMain.RUnlock()
 	item := c.items[key]
 	if item != nil {
-		if len(c.items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> moving on set! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
+		c.muQueue.Lock()
+		defer c.muQueue.Unlock()
+		return c.lruCache.getItem(item), true
+	}
+	return nil, false
+}
+
+func (c *lruSafeCache) Clear() {
+	c.muMain.Lock()
+	c.lruCache.Clear()
+	c.muMain.Unlock()
+}
+
+func (c *lruCacheSingleLock) Set(key Key, value interface{}) bool {
+	// we need to make sure when we move to front item is not deleted from queue
+	c.muMain.Lock()
+	item := c.items[key]
+	if item != nil {
+		// when deleting we need to make sure muMap is locked
 		c.lruCache.setItem(item, value)
+		c.muMain.Unlock()
 		return true
 	}
 
-	if len(c.items) > c.capacity {
-		fmt.Printf("(%07d) [%s] -> clearing and adding! (queue [%s], items [%s])\n",
-			id, key, printQueue(c.queue), printMap(c.items))
+	if c.queue.Len() == c.capacity {
+		c.removeLastItem()
 	}
-	c.helper.clearAndAddItem(c.lruCache, key, value)
+	c.addItem(key, value)
+	c.muMain.Unlock()
 	return false
 }
 
-func (c *lruItemCacheDoubleLock) Set(key Key, value interface{}) bool {
-	id := rand.Intn(1_000_000)
+func (c *lruCacheDoubleLock) Set(key Key, value interface{}) bool {
 	// we need to make sure when we move to front item is not deleted from queue
 	c.muMain.RLock()
 	item := c.items[key]
 	if item != nil {
+		// when deleting we need to make sure muMap is locked
 		c.muQueue.Lock()
-		if len(c.items) > c.capacity {
-			fmt.Printf("(%07d) [%s] -> moving on set! (queue [%s], items [%s])\n",
-				id, key, printQueue(c.queue), printMap(c.items))
-		}
 		c.lruCache.setItem(item, value)
 		c.muQueue.Unlock()
 		c.muMain.RUnlock()
@@ -397,37 +263,76 @@ func (c *lruItemCacheDoubleLock) Set(key Key, value interface{}) bool {
 	c.muMain.RUnlock()
 
 	c.muMain.Lock()
-	if len(c.items) > c.capacity {
-		fmt.Printf("(%07d) [%s] -> clearing and adding! (queue [%s], items [%s])\n",
-			id, key, printQueue(c.queue), printMap(c.items))
+	for c.queue.Len() >= c.capacity {
+		c.removeLastItem()
 	}
-	c.helper.clearAndAddItem(c.lruCache, key, value)
+
+	items := c.items
+	if _, exists := items[key]; !exists {
+		c.addItem(key, value)
+	}
 	c.muMain.Unlock()
 	return false
 }
 
-func printQueue(l List) string {
-	elems := make([]string, 0, l.Len())
-	for item := l.Front(); item != nil; item = item.Next {
-		elems = append(elems, string(item.Value.(*elem).key))
+func (c *lruCacheTripleLock) Set(key Key, value interface{}) bool {
+	// we need to make sure when we move to front item is not deleted from queue
+	c.muMain.RLock()
+	item := c.items[key]
+	if item != nil {
+		// when deleting we need to make sure muMap is locked
+		c.muQueue.Lock()
+		c.lruCache.setItem(item, value)
+		c.muQueue.Unlock()
+		c.muMain.RUnlock()
+		return true
 	}
-	return strings.Join(elems, ", ")
+	c.muMain.RUnlock()
+
+	c.muMain.Lock()
+	for c.queue.Len() >= c.capacity {
+		c.removeLastItem()
+	}
+	c.muMain.Unlock()
+
+	c.muMain.Lock()
+	items := c.items
+	if _, exists := items[key]; !exists {
+		c.addItem(key, value)
+	}
+	c.muMain.Unlock()
+	return false
 }
 
-func printMap(items map[Key]*ListItem) string {
-	keys := make([]string, 0, len(items))
-	for k := range items {
-		keys = append(keys, string(k))
+func (c *lruItemCacheSingleLock) Set(key Key, value interface{}) bool {
+	// we need to make sure when we move to front item is not deleted from queue
+	c.muMain.Lock()
+	defer c.muMain.Unlock()
+	item := c.items[key]
+	if item != nil {
+		c.lruCache.setItem(item, value)
+		return true
 	}
-	return strings.Join(keys, ", ")
+
+	c.helper.clearAndAddItem(c.lruCache, key, value)
+	return false
 }
 
-// variants - 1 lock. 2 locks, 3 locks, handle single cap
+func (c *lruItemCacheDoubleLock) Set(key Key, value interface{}) bool {
+	// we need to make sure when we move to front item is not deleted from queue
+	c.muMain.RLock()
+	item := c.items[key]
+	if item != nil {
+		c.muQueue.Lock()
+		c.lruCache.setItem(item, value)
+		c.muQueue.Unlock()
+		c.muMain.RUnlock()
+		return true
+	}
+	c.muMain.RUnlock()
 
-// common lock or remove all extra if more
-// lookup or lock both
-// if more goroutines than cap - it can fill the cache and it will be bigger
-// 1 do or not
-// 1 with many goroutines
-// test 1, 3, 10
-// more values than goroutines || cap > goroutines
+	c.muMain.Lock()
+	c.helper.clearAndAddItem(c.lruCache, key, value)
+	c.muMain.Unlock()
+	return false
+}
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestCache(t *testing.T) {
 	t.Run("empty cache", func(t *testing.T) {
-		c := NewCache(10, NoLock)
+		c := NewCache(NoLock, 10)
 
 		_, ok := c.Get("aaa")
 		require.False(t, ok)
@@ -22,7 +22,7 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("simple", func(t *testing.T) {
-		c := NewCache(5, NoLock)
+		c := NewCache(NoLock, 5)
 
 		wasInCache := c.Set("aaa", 100)
 		require.False(t, wasInCache)
@@ -54,7 +54,7 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("clearing cache", func(t *testing.T) {
-		c := NewCache(3, NoLock)
+		c := NewCache(NoLock, 3)
 
 		wasInCache := c.Set("aaa", 100)
 		require.False(t, wasInCache)
@@ -106,7 +106,7 @@ func TestCache(t *testing.T) {
 
 func TestCachePurgeLogic(t *testing.T) {
 	t.Run("purged on set", func(t *testing.T) {
-		c := NewCache(3, NoLock)
+		c := NewCache(NoLock, 3)
 		c.Set("aaa", 100)
 		c.Set("bbb", 200)
 		c.Set("ccc", 300)
@@ -132,7 +132,7 @@ func TestCachePurgeLogic(t *testing.T) {
 	})
 
 	t.Run("complex purge logic", func(t *testing.T) {
-		c := NewCache(3, NoLock)
+		c := NewCache(NoLock, 3)
 		c.Set("aaa", 100)
 		c.Set("bbb", 200)
 		c.Set("ccc", 300)
@@ -248,62 +248,62 @@ const (
 )
 
 func TestCacheMultithreading(t *testing.T) {
-	c := NewCache(10, TripleLock)
-	runner(t, c, 2, 1, maxRandom, false)
+	c := NewCache(TripleLock, 10)
+	runner(t, c, 1, 1, maxRandom, false)
 	require.Equal(t, 10, c.Len())
 }
 
 func TestCacheMultithreading2Lock(t *testing.T) {
-	c := NewCache(10, DoubleLock)
-	runner(t, c, 2, 1, maxRandom, false)
+	c := NewCache(DoubleLock, 10)
+	runner(t, c, 1, 1, maxRandom, false)
 	require.Equal(t, 10, c.Len())
 }
 
 func TestCacheMultithreading1Lock(t *testing.T) {
-	c := NewCache(10, SingleLock)
-	runner(t, c, 2, 1, maxRandom, false)
+	c := NewCache(SingleLock, 10)
+	runner(t, c, 1, 1, maxRandom, false)
 	require.Equal(t, 10, c.Len())
 }
 
 func TestCacheMultithreadingClear(t *testing.T) {
-	c := NewCache(10, TripleLock)
-	runner(t, c, 2, 1, maxRandom, true)
+	c := NewCache(TripleLock, 10)
+	runner(t, c, 1, 1, maxRandom, true)
 	require.LessOrEqual(t, c.Len(), 10)
 }
 
 func TestCacheMultithreadingClear2Lock(t *testing.T) {
-	c := NewCache(10, DoubleLock)
-	runner(t, c, 2, 1, maxRandom, true)
+	c := NewCache(DoubleLock, 10)
+	runner(t, c, 1, 1, maxRandom, true)
 	require.LessOrEqual(t, c.Len(), 10)
 }
 
 func TestCacheMultithreadingClear1Lock(t *testing.T) {
-	c := NewCache(10, SingleLock)
-	runner(t, c, 2, 1, maxRandom, true)
+	c := NewCache(SingleLock, 10)
+	runner(t, c, 1, 1, maxRandom, true)
 	require.LessOrEqual(t, c.Len(), 10)
 }
 
 func TestCacheMultithreadingOnly(t *testing.T) {
 	c := NewItemCache(DoubleLock)
-	runner(t, c, 2, 1, 5, false)
+	runner(t, c, 1, 1, 5, false)
 	require.Equal(t, 1, c.Len())
 }
 
 func TestCacheMultithreadingOnlyClear(t *testing.T) {
 	c := NewItemCache(DoubleLock)
-	runner(t, c, 2, 1, 5, true)
+	runner(t, c, 1, 1, 5, true)
 	require.LessOrEqual(t, c.Len(), 1)
 }
 
 func TestCacheMultithreadingSmall(t *testing.T) {
-	c := NewCache(3, TripleLock)
-	runner(t, c, 2, 1, 5, false)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 1, 1, 5, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreadingSmallClear(t *testing.T) {
-	c := NewCache(3, TripleLock)
-	runner(t, c, 2, 1, 5, true)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 1, 1, 5, true)
 	require.LessOrEqual(t, c.Len(), 3)
 }
 
@@ -311,20 +311,20 @@ func TestCacheMultithreading10GoroutinesKeysLessThanSetters(t *testing.T) {
 	// this test will fail because triple lock cache is not made for caching data
 	// when we have number of possible keys less or equal than number of goroutines
 	t.Skip()
-	c := NewCache(3, TripleLock)
-	runner(t, c, 10, 5, 4, false)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, 4, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysLessThanSetters2Lock(t *testing.T) {
-	c := NewCache(3, DoubleLock)
-	runner(t, c, 10, 5, 4, false)
+	c := NewCache(DoubleLock, 3)
+	runner(t, c, 5, 5, 4, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysLessThanSetters1Lock(t *testing.T) {
-	c := NewCache(3, SingleLock)
-	runner(t, c, 10, 5, 4, false)
+	c := NewCache(SingleLock, 3)
+	runner(t, c, 5, 5, 4, false)
 	require.Equal(t, 3, c.Len())
 }
 
@@ -332,8 +332,8 @@ func TestCacheMultithreading10GoroutinesKeysLikeSetters(t *testing.T) {
 	// this test will fail because triple lock cache is not made for caching data
 	// when we have number of possible keys less or equal than number of goroutines
 	t.Skip()
-	c := NewCache(3, TripleLock)
-	runner(t, c, 10, 5, 5, false)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, 5, false)
 	require.Equal(t, 3, c.Len())
 }
 
@@ -342,86 +342,96 @@ func TestCacheMultithreading10GoroutinesKeysLikeSettersClear(t *testing.T) {
 	// when we have number of possible keys less or equal than number of goroutines
 	// the cache is cleared, but it may still saturate on the last run
 	t.Skip()
-	c := NewCache(3, TripleLock)
-	runner(t, c, 10, 5, 5, true)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, 5, true)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysLikeSetters2Lock(t *testing.T) {
-	c := NewCache(3, DoubleLock)
-	runner(t, c, 10, 5, 5, false)
+	c := NewCache(DoubleLock, 3)
+	runner(t, c, 5, 5, 5, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysLikeSetters1Lock(t *testing.T) {
-	c := NewCache(3, SingleLock)
-	runner(t, c, 10, 5, 5, false)
+	c := NewCache(SingleLock, 3)
+	runner(t, c, 5, 5, 5, false)
+	require.Equal(t, 3, c.Len())
+}
+
+func TestCacheMultithreading10GoroutinesKeysOneMoreThanSetters(t *testing.T) {
+	t.Skip()
+	// this test may fail because triple lock cache is not made for caching data
+	// when we have number of possible keys a bit higher than number of goroutines
+	// in this case the cache may saturate too
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, 6, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysMoreThanSetters(t *testing.T) {
-	c := NewCache(3, TripleLock)
-	runner(t, c, 10, 5, 6, false)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, 7, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysMoreThanSetters2Lock(t *testing.T) {
-	c := NewCache(3, DoubleLock)
-	runner(t, c, 10, 5, 6, false)
+	c := NewCache(DoubleLock, 3)
+	runner(t, c, 5, 5, 6, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesKeysMoreThanSetters1Lock(t *testing.T) {
-	c := NewCache(3, SingleLock)
-	runner(t, c, 10, 5, 6, false)
+	c := NewCache(SingleLock, 3)
+	runner(t, c, 5, 5, 6, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestCacheMultithreading10GoroutinesClear(t *testing.T) {
-	c := NewCache(3, TripleLock)
-	runner(t, c, 10, 5, maxRandom, true)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, maxRandom, true)
 	require.LessOrEqual(t, c.Len(), 3)
 }
 
 func TestCacheMultithreading10GoroutinesClear2Lock(t *testing.T) {
-	c := NewCache(3, DoubleLock)
-	runner(t, c, 10, 5, maxRandom, true)
+	c := NewCache(DoubleLock, 3)
+	runner(t, c, 5, 5, maxRandom, true)
 	require.LessOrEqual(t, c.Len(), 3)
 }
 
 func TestCacheMultithreading10GoroutinesClear1Lock(t *testing.T) {
-	c := NewCache(3, SingleLock)
-	runner(t, c, 10, 5, maxRandom, true)
+	c := NewCache(SingleLock, 3)
+	runner(t, c, 5, 5, maxRandom, true)
 	require.LessOrEqual(t, c.Len(), 3)
 }
 
 func TestSingleLockCache(t *testing.T) {
-	c := NewCache(3, SingleLock)
-	runner(t, c, 10, 5, 100, false)
+	c := NewCache(SingleLock, 3)
+	runner(t, c, 5, 5, 100, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestDoubleLockCache(t *testing.T) {
-	c := NewCache(3, DoubleLock)
-	runner(t, c, 10, 5, 100, false)
+	c := NewCache(DoubleLock, 3)
+	runner(t, c, 5, 5, 100, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestTripleLockCache(t *testing.T) {
-	c := NewCache(3, TripleLock)
-	runner(t, c, 10, 5, 100, false)
+	c := NewCache(TripleLock, 3)
+	runner(t, c, 5, 5, 100, false)
 	require.Equal(t, 3, c.Len())
 }
 
 func TestItemSingleLockCache(t *testing.T) {
 	c := NewItemCache(SingleLock)
-	runner(t, c, 10, 5, 100, false)
+	runner(t, c, 5, 5, 100, false)
 	require.Equal(t, 1, c.Len())
 }
 
 func TestItemDoubleLockCache(t *testing.T) {
 	c := NewItemCache(DoubleLock)
-	runner(t, c, 10, 5, 100, false)
+	runner(t, c, 5, 5, 100, false)
 	require.Equal(t, 1, c.Len())
 }
 
@@ -445,20 +455,16 @@ func setter(wg *sync.WaitGroup, c Cache, count int) {
 	}
 }
 
-func runner(tb testing.TB, c Cache, routines, readers, keys int, clearCache bool) {
+func runner(tb testing.TB, c Cache, readers, writers, keys int, clearCache bool) {
 	tb.Helper()
-	if readers > routines {
-		tb.Fatal("More readers than routines")
-	}
-
 	if keys > maxRandom {
 		tb.Fatalf("Too many keys, should be less than %d", maxRandom)
 	}
 
 	wg := &sync.WaitGroup{}
-	wg.Add(routines)
+	wg.Add(readers + writers)
 
-	for range make([]struct{}, routines-readers) {
+	for range make([]struct{}, writers) {
 		go setter(wg, c, keys)
 	}
 
@@ -469,26 +475,44 @@ func runner(tb testing.TB, c Cache, routines, readers, keys int, clearCache bool
 }
 
 func BenchmarkSingleLockCache(b *testing.B) {
-	c := NewCache(10, SingleLock)
-	runner(b, c, 10, 5, 100, false)
+	benchmarkCache(b, SingleLock, 10)
 }
 
 func BenchmarkDoubleLockCache(b *testing.B) {
-	c := NewCache(10, DoubleLock)
-	runner(b, c, 10, 5, 100, false)
+	benchmarkCache(b, DoubleLock, 10)
 }
 
 func BenchmarkTripleLockCache(b *testing.B) {
-	c := NewCache(10, TripleLock)
-	runner(b, c, 10, 5, 100, false)
+	benchmarkCache(b, TripleLock, 10)
 }
 
 func BenchmarkItemSingleLockCache(b *testing.B) {
-	c := NewItemCache(SingleLock)
-	runner(b, c, 10, 5, 100, false)
+	benchmarkItemCache(b, SingleLock)
 }
 
 func BenchmarkItemDoubleLockCache(b *testing.B) {
-	c := NewItemCache(DoubleLock)
-	runner(b, c, 10, 5, 100, false)
+	benchmarkItemCache(b, DoubleLock)
+}
+
+func benchmarkCache(b *testing.B, kind Kind, capacity int) {
+	b.Helper()
+
+	c := NewCache(kind, capacity)
+	b.ResetTimer()
+	for range b.N {
+		runner(b, c, 5, 5, 100, false)
+		c.Clear()
+	}
+}
+
+func benchmarkItemCache(b *testing.B, kind Kind) {
+	b.Helper()
+
+	c := NewItemCache(kind)
+	b.ResetTimer()
+	for range b.N {
+		runner(b, c, 5, 5, 100, false)
+		c.Clear()
+	}
+	b.Helper()
 }
